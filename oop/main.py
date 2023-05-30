370208c7aa402fc1eea3d89fa57c4c301e4346cd
Louis Agyapong <47106199+louis-agyapong@users.noreply.github.com>
2023-05-30 19:09:37 +0000

    alternative constructors (classmethod)

--- oop/main.py
+++ oop/main.py
@@ -10,18 +10,30 @@ class Employee:
 
         Employee.num_of_emps += 1
 
-    def get_fullname(self) -> str:
+    def fullname(self) -> str:
         return f"{self.first} {self.last}"
 
     def apply_raise(self):
         self.pay = int(self.pay * Employee.raise_amount)
         return self.pay
 
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amount = amount
 
-emp_1 = Employee("Louis", "Agyapong", 6000)
-emp_2 = Employee("Zion", "Agyapong", 10000)
+    @classmethod
+    def fromstring(cls, emp_str):
+        """Alternative constructors"""
+        first, last, pay = emp_str.split("-")
+        return cls(first, last, pay)
 
-print(emp_1.apply_raise())
-emp_1.raise_amount = 1.05
-print(emp_1.apply_raise())
-print(Employee.num_of_emps)
+
+emp_str_1 = "Zion-Kwaku-5000"
+emp_str_2 = "Afia-Sika-4000"
+emp_str_3 = "Maame-Afia-30000"
+emp_str_4 = "Paa-Yaw-50000"
+
+new_emp_1 = Employee.fromstring(emp_str_1)
+
+print(new_emp_1.fullname())
+print(new_emp_1.pay)
