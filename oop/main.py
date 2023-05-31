ea6db3b63a4a2fa68029415f715d248047b8b506
Louis Agyapong <47106199+louis-agyapong@users.noreply.github.com>
2023-05-31 11:57:29 +0000

    code refactor

--- oop/main.py
+++ oop/main.py
@@ -48,16 +48,16 @@ class Employee:
         return True
 
     def __repr__(self) -> str:
-        return f"{self.fullname()} {self.pay}"
+        return f"{self.fullname} {self.pay}"
 
     def __str__(self) -> str:
-        return f"{self.fullname()} {self.email}"
+        return f"{self.fullname} {self.email}"
 
     def __add__(self, other):
         return self.pay + other.pay
 
     def __len__(self):
-        return len(self.fullname())
+        return len(self.fullname)
 
 
 class Developer(Employee):
